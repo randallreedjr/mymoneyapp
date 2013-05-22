060dfc55899a547b5de42c6f6cb4818daaf8427a
Randall Reed, Jr <randallreedjr@gmail.com>
2013-05-22 13:10:33 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mymoneyapp::Application.config.secret_token = '9236b7b7dfe52b1164632a2fd0c223ebd78ffa1f5a3a9478f1cad4893c8ff74daa2cffa6da22130b50a1a566230abe3371152781a88667843894e6a3df318d8e'
