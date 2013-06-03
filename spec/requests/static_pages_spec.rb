59fbb3157cf1ffe1d751ad01551cd958bb9f801d
Randall Reed, Jr <randallreedjr@gmail.com>
2013-06-03 13:10:03 -0400

    Finished contact page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -42,4 +42,17 @@ describe "Static pages" do
 				:text => "Money Garden | About")
 		end
 	end
+
+	describe "Contact page" do
+		it "should have the h1 'Contact Us'" do
+			visit '/static_pages/contact'
+			page.should have_selector('h1', :text => 'Contact Us')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/contact'
+			page.should have_selector('title', 
+				:text => "Money Garden | Contact")
+		end
+	end
 end
