2d566db2f2a4d2bb7e6d37b70ebdac592f7c14e2
Randall Reed, Jr <randallreedjr@gmail.com>
2013-06-03 12:58:09 -0400

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+	describe "Home page" do
+		it "should have the h1 'Money Garden'" do
+			visit '/static_pages/home'
+			page.should have_selector('h1', :text => 'Money Garden')
+    	end
+
+    	it "should have the right title" do
+    		visit '/static_pages/home'
+    		page.should have_selector('title', 
+    			:text => "Money Garden | Home")
+    	end
+  	end
+
+
+	describe "Help page" do
+
+		it "should have the h1 'Help'" do
+			visit '/static_pages/help'
+			page.should have_selector('h1', :text => 'Help')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/help'
+			page.should have_selector('title', 
+				:text => "Money Garden | Help")
+		end
+	end
+
+	describe "About page" do
+		it "should have the h1 'About Us'" do
+			visit '/static_pages/about'
+			page.should have_selector('h1', :text => 'About Us')
+		end
+
+		it "should have the right title" do
+			visit '/static_pages/about'
+			page.should have_selector('title', 
+				:text => "Money Garden | About")
+		end
+	end
+end
