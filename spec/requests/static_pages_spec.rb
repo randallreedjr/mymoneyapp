86604abbd0dee35a1f06f6c50c1e304b4da7292f
Randall Reed, Jr <randallreedjr@gmail.com>
2013-06-03 18:21:30 -0400

    Updated testing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+	let(:base_title) { "Money Garden" }
+
 	describe "Home page" do
 		it "should have the h1 'Money Garden'" do
 			visit '/static_pages/home'
@@ -11,7 +13,7 @@ describe "Static pages" do
     	it "should have the right title" do
     		visit '/static_pages/home'
     		page.should have_selector('title', 
-    			:text => "Money Garden | Home")
+    			:text => "#{base_title} | Home")
     	end
   	end
 
@@ -26,7 +28,7 @@ describe "Static pages" do
 		it "should have the right title" do
 			visit '/static_pages/help'
 			page.should have_selector('title', 
-				:text => "Money Garden | Help")
+				:text => "#{base_title} | Help")
 		end
 	end
 
@@ -39,7 +41,7 @@ describe "Static pages" do
 		it "should have the right title" do
 			visit '/static_pages/about'
 			page.should have_selector('title', 
-				:text => "Money Garden | About")
+				:text => "#{base_title} | About")
 		end
 	end
 
@@ -52,7 +54,7 @@ describe "Static pages" do
 		it "should have the right title" do
 			visit '/static_pages/contact'
 			page.should have_selector('title', 
-				:text => "Money Garden | Contact")
+				:text => "#{base_title} | Contact")
 		end
 	end
 end
