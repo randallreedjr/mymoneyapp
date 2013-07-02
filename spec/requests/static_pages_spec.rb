b9e9d00da6fd667b8ddea0865d33cfeafaf67f1b
Randall Reed, Jr <randallreedjr@gmail.com>
2013-07-02 12:55:28 -0400

    Update rspec with before blocks

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,19 +5,18 @@ describe "Static pages" do
 	let(:base_title) { "Money Garden" }
 
 	describe "Home page" do
+		before { visit root_path }
+
 		it "should have the h1 'Money Garden'" do
-			visit root_path
 			page.should have_selector('h1', :text => 'Money Garden')
     	end
 
     	it "should have the base title" do
-    		visit root_path
     		page.should have_selector('title', 
     			:text => "Money Garden")
     	end
 
     	it "should not have a custom page title" do
-    		visit root_path
     		page.should_not have_selector('title', 
     			:text => "| Home")
     	end
@@ -25,40 +24,39 @@ describe "Static pages" do
 
 
 	describe "Help page" do
+		before { visit help_path }
 
 		it "should have the h1 'Help'" do
-			visit help_path
 			page.should have_selector('h1', :text => 'Help')
 		end
 
 		it "should have the right title" do
-			visit help_path
 			page.should have_selector('title', 
 				:text => "#{base_title} | Help")
 		end
 	end
 
 	describe "About page" do
+		before { visit about_path }
+
 		it "should have the h1 'About Us'" do
-			visit about_path
 			page.should have_selector('h1', :text => 'About Us')
 		end
 
 		it "should have the right title" do
-			visit about_path
 			page.should have_selector('title', 
 				:text => "#{base_title} | About")
 		end
 	end
 
 	describe "Contact page" do
+		before { visit contact_path }
+
 		it "should have the h1 'Contact Us'" do
-			visit contact_path
 			page.should have_selector('h1', :text => 'Contact Us')
 		end
 
 		it "should have the right title" do
-			visit contact_path
 			page.should have_selector('title', 
 				:text => "#{base_title} | Contact")
 		end
