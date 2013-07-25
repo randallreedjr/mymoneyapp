ceeecb8d114f2c2f22650d61dc1039e5c79eb607
Randall Reed, Jr <randallreedjr@gmail.com>
2013-07-24 23:22:52 -0400

    Block new and create actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user, only: [:edit, :update]
   before_filter :admin_user, only: :destroy
+  before_filter :signed_in_user_action, only: [:new, :create]
 
   def show
   	@user = User.find(params[:id])
@@ -66,5 +67,11 @@ class UsersController < ApplicationController
 
     def admin_user
       redirect_to(root_path) unless current_user.admin?
+    end
+
+    def signed_in_user_action
+      if signed_in?
+        redirect_to root_path, notice: "Already logged in"
+      end
     end
 end
