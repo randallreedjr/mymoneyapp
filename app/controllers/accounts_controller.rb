28736d222e9195a4552cf438b63d0bc4b8f9d998
Randall Reed, Jr <randallreedjr@gmail.com>
2013-10-24 00:38:27 -0400

    Add before filter to accounts controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,9 @@
 class AccountsController < ApplicationController
-	before_filter :signed_in_user
+	before_filter :signed_in_user, only: [:create, :destroy]
 
+	def index
+	end
+	
 	def create
 	end
 
