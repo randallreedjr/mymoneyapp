36e63e967d1ed99b1e45a35dae52cfeaeebf0f06
Randall Reed, Jr <randallreedjr@gmail.com>
2013-11-06 19:11:22 -0500

    Correct typo in 'Profile updates'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
   def update
     if @user.update_attributes(params[:user])
-      flash[:success] = "Profile updates"
+      flash[:success] = "Profile updated"
       sign_in @user
       redirect_to @user
     else
